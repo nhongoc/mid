3a85f004b9ba43b570885dafde84fc82f2c027d9
Laura Slayton <laura.slayton.19@cnu.edu>
2020-11-20 13:57:28 -0500

    done

--- src/327_mid1_1.cpp
+++ src/327_mid1_1.cpp
@@ -10,51 +10,12 @@ using namespace std;
 //TODO 20 pts please move everything in the KP namespace into utils.h and utils.cpp
 //            be sure everything remains in namespace KP
 
-namespace KP{
-	const std::string FILENAME = "input.txt";
-	struct person{
-		std::string name;
-	};
-
-
-	//TODO 5 pts define function
-	/**
-	 * counts and returns the number of people with the name personname in fvec
-	 * @param pvec: a vector that holds person structs
-	 *        prsnname: a persons name
-	 * @return the number people with the name prsnname
-	 */
-	int getcount(vector<person> &pvec,std::string &prsnname);
-
-
-	//TODO 5 pts define function
-	/**
-	 * 	asks the user for a name, reads it in and then returns it
-	 * @param none
-	 * @return the name the user entered
-	 */
-	std::string getonename();
-
-	//TODO 10 pts define function
-	/**
-	 * tries to open file fn, read 1 line from it, and return this line
-	 * @param fn  filename where a name should be, file may not be present
-	 * @return the contents of the file, or an empty string if file not found
-	 */
-	std::string readfile(const std::string &fn=FILENAME);
-
-	//TODO 10 pts create a function
-	/**
-	 * tries to open file fn, write info to it
-	 * @param info what to write to the file
-	 * @param sfn  filename where a name should be, file may not be present
-	 * @return the contents of the file, or an empty string if file not found
-	 */
-	bool writefile(std::string &info, const std::string &fn=FILENAME);
-}
-
 //NOTE please do not modify anything in main
+
+
 int main(){
+
+
     //holds person structs
     vector<KP::person> fvec;
 
